3d01060112dfbdff35c6dfe3845bef9dcd5ac72e
Hugo Domingos Pina <hugopina99@gmail.com>
2024-03-18 21:53:54 -0300

    Att

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,22 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const listicon = document.querySelector(".listicon");
-  const nav = document.querySelector(".navbar-style");
+// document.addEventListener("DOMContentLoaded", function() {
+//   const listicon = document.querySelector(".listicon");
+//   const nav = document.querySelector(".navbar-style");
 
-  listicon.addEventListener("click", () => {
-      nav.classList.toggle("active");
-  });
+//   listicon.addEventListener("click", () => {
+//       nav.classList.toggle("active");
+//   });
+// });
+
+const links = document.querySelector(".navbar-style");
+const listicon = document.querySelector(".listicon");
+
+listicon.addEventListener("click", () =>{
+  links.classList.toggle("active");
+});
+
+const links1 = document.querySelector(".person-style");
+const picon = document.querySelector(".picon");
+
+picon.addEventListener("click", () =>{
+  links1.classList.toggle("open");
 });
